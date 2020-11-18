7958eed38d13f148129cf709e40f0e5350f3942b
nehaldhumal <72104019+nehaldhumal@users.noreply.github.com>
2020-11-19 00:00:44 +0530

    Create number_operations.py

---
+++ python_programs/number_operations.py
@@ -0,0 +1,58 @@
+# int and floats
+
+num = 3
+print(type(num))
+
+# Arithmetic operators
+# Addition: 3 + 2
+# Subtraction: 3 - 2
+# Multiplication: 3 * 2
+# Division: 3 / 2
+# Floor Division: 3 // 2
+# Exponent: 3 ** 2
+# Modulus: 3 % 2
+
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print(3 // 2)
+print(3 ** 2)
+print(3 % 2)
+print(3+(2-1))
+
+# increement a value
+num = 1
+num = num + 1
+num += 10
+print(num)
+
+# absolute value abs : basically this will just remove the sign from any negative numbers
+# round function
+print(abs(-3))
+print(round(3.75))
+print(round(3.75,1))
+
+# Comparisons Operators:
+# Equal:   3 == 2
+# Not Equal:  3 != 2
+# Greater Than: 3 > 2
+# Less Than: 3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal: 3 <= 2
+
+num1 = 3
+num2 = 2
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 >= num2)
+print(num1 <= num2)
+
+num1 = '100'
+num2 = '200'
+print(num1 + num2)
+num1 = int(num1)          # casting - casting str to int
+num2 = int(num2)          # casting - casting str to int
+print(num1 + num2)
